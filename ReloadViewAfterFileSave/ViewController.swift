3fac76fd279be7e9b12b7e7d0253aae6b4fa264d
Nayem BJIT <nayem@Mac-mini.local>
2017-06-15 16:40:08 +0600

    First Working Copy

--- ReloadViewAfterFileSave/ViewController.swift
+++ ReloadViewAfterFileSave/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(updateUI), name: Notification.Name("FileWrittenCompletion"), object: nil)
+        myLabel.text = "File is being saved"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func updateUI() {
+//        DispatchQueue.main.async {
+            self.myLabel.textColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            self.myLabel.text = "File is saved successfully"
+            self.view.setNeedsDisplay()
+//        }
+    }
 
 }
 
